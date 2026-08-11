db03e80469c59793bd9d688ee86c62478372f940
thellcishii-lang <the.llcishii@gmail.com>
2026-08-12 00:06:48 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// app/page.tsx
+import { redirect } from 'next/navigation';
+import { getCurrentStaff } from '@/lib/utils/auth';
+
+export default async function HomePage() {
+  const staff = await getCurrentStaff();
+
+  if (staff) {
+    redirect('/staff/dashboard');
+  } else {
+    redirect('/login');
+  }
+}
